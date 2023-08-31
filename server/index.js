92ebbf83d7648a2c3d3b8fc8e53f44d8643251a5
rayzhou4 <rayzhou4@gmail.com>
2023-08-30 23:18:41 -0600

    commit

--- server/index.js
+++ server/index.js
@@ -77,4 +77,8 @@ app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
 
-// app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../build')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../build', 'index.html'));
+// });
